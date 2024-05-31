c6c6dc16dd7745f2b2206a457f94841da044f68a
Azkhal20 <azkalzavier@gmail.com>
2024-05-31 13:48:33 +0700

    build main menu: add assett

--- js/game.js
+++ js/game.js
@@ -1,13 +1,24 @@
 setGame("1200x600");
 game.folder = "assets";
 //file gambar yang dipakai dalam game
-var gambar = {
+const gambar = {
 	logo:"logo.png",
-	startBtn:"tombolStart.png",
-	cover:"cover.jpg",
-	playBtn:"btn-play.png",
+	startBtn:"startBtn.png",
+	cover:"cover.png",
+	playBtn:"playBtn.png",
 	maxBtn:"maxBtn.png",
-	minBtn:"minBtn.png"	
+	minBtn:"minBtn.png",
+	closeBtn:"closeBtn.png",
+	pauseBtn:"pauseBtn.png",
+	musicBtn:"musicBtn.png",
+	startLine:"startLine.png",
+	finishLine:"finishLine.png",
+	settingBtn:"settingBtn.png",
+	windowSetting:"windowSetting.png",
+	headerSetting:"headerSetting.png",
+	car1:"car1.png",
+	indicatorOn:"indicatorOn.png",
+	indicatorOff:"indicatorOff.png",
 }
 //file suara yang dipakai dalam game
 var suara = {
@@ -17,7 +28,7 @@ var suara = {
 loading(gambar, suara, startScreen);
 
 function startScreen(){	
-	hapusLayar("#67d2d6");
+	hapusLayar("#577B8D");
 	tampilkanGambar(dataGambar.logo, 600, 250);
 	var startBtn = tombol(dataGambar.startBtn, 600, 350);
 	if (tekan(startBtn)){
@@ -27,12 +38,24 @@ function startScreen(){
 function halamanCover(){
 	hapusLayar("#67d2d6");
 	gambarFull(dataGambar.cover);
-	var playBtn = tombol(dataGambar.playBtn, 1100, 500);
+	let playBtn = tombol(dataGambar.playBtn, 600, 450);
 	if (tekan(playBtn)){
 		setAwal();
 		jalankan(gameLoop);
 	}	
 	resizeBtn(1150,50);
+
+	// Tombol CLose
+	// let closeBtn = tombol(dataGambar.closeBtn, 50, 50);
+	// if (tekan(closeBtn)){
+	// 	jalankan(startScreen);
+	// }
+
+	// Tombol Setting (blom jadi)
+	let settingBtn = tombol(dataGambar.settingBtn, 50, 50);
+	if (tekan(settingBtn)){
+		jalankan(windowSetting, 600, 400);
+	}
 }
 
 function setAwal(){
